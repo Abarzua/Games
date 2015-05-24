192d70e7637e281865ff45076314e34913c2a735
AbarzuaLuis <luis.abarzua.ing@gmail.com>
2015-05-24 05:21:34 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -153,7 +153,7 @@ $apellidos = $fila['apellidos'];
                     <small>Page Not Found</small>
                 </h1>
                 <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
+                    <li><a href="home.php">Home</a>
                     </li>
                     <li class="active">404</li>
                 </ol>
